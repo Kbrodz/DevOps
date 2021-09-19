206b5bae5b365a890f443c93bb91b8d7fe94882d
Konrad Brodziak <48966539+Kbrodz@users.noreply.github.com>
2021-09-19 15:14:05 +0200

    Add files via upload

--- mybackend/index.js
+++ mybackend/index.js
@@ -1,49 +1,32 @@
-const bodyParser = require('body-parser')
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 const app = express();
-const db = require('./queries')
 
-app.use(bodyParser.urlencoded({ extended: false }))
- 
-// parse application/json
-app.use(bodyParser.json())
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-const redis = require('redis');
-
+const redis = require("redis");
 const redisClient = redis.createClient({
-host: "myredis",
-port:6379,
-retry_startegy: () => 1000
-}); 
-
-const { Pool } = require('pg');
-
-const pgClient = new Pool({ 
-    user:"postgres",
-    password:"12345678A!",
-    database:"postgres",
-    host:"mypostgres",
-    port:"5432"
-})
-
-pgClient.on('connect',() => {
-    console.log(`Postgress connected`) 
+    host: "myredis",
+    port: 6379,
+    //retryStrategy: () => 1000
 });
 
-pgClient.query('CREATE TABLE IF NOT EXISTS users (ID SERIAL PRIMARY KEY, name varchar(30) , email varchar(50), cash INT)').catch((err) =>{
-console.log(`Nie udało się`) 
+redisClient.on('connect', () => {
+    console.log("Connected to Redis server");
 });
 
-app.get("/", (req,res) => {res.send("Hello World ")});
-app.get('/users', db.getUsers)
-app.get('/users/:id',db.cache, db.getUserById)
-app.post('/users', db.createUser)
-app.put('/users/:id', db.updateUser)
-app.delete('/users/:id', db.deleteUser)
-
-const PORT = 5000;
-
-app.listen(PORT, () =>{
-    console.log(`API listening on port ${PORT}`) 
+redisClient.on('error', (error) => {
+    console.log(error);
 });
+
+app.get('/hello', (req, res) => {
+    res.send("Hello World from express server!");
+});
+
+const PORT = 4000;
+app.listen(PORT, () => {
+    console.log(`API listening on port ${PORT}`)
+});
+
